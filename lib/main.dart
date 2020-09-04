783881f5096e24ca1738e4d400a6278c2fa0911c
patricia.damasceno <patricia.damasceno>
2020-09-04 09:22:15 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:contactlist/ui/contact_page.dart';
+import 'package:contactlist/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
